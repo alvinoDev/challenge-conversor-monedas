a895ec6ec943eb47e73bf410f04297b33319f2a3
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-10-29 09:04:19 -0400

    Revert "feat: primera version de la App de consola"
    This reverts commit 99fdb322b4129bb9c778053ed16d15d01a21e09d.

--- src/com/alvinodev/currencyconverter/model/ConversionResponse.java
+++
@@ -1,9 +0,0 @@
-package com.alvinodev.currencyconverter.model;
-
-public record ConversionResponse(String base_code, String target_code, double conversion_result) {
-    @Override
-    public String toString() {
-        // Formato para mostrar el resultado: "corresponde al valor final de =>> 20293.75 [ARS]"
-        return String.format("corresponde al valor final de =>> %.2f [%s]", conversion_result, target_code);
-    }
-}
